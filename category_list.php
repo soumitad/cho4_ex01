258e1a61b20dd866e3ee9e87f5e7a652e45372ab
Soumita Das <sd655@njit.edu>
2017-02-23 22:10:10 -0500

    Add categories

--- category_list.php
+++ category_list.php
@@ -47,29 +47,15 @@ $statement->closeCursor();
     <h2>Add Category</h2>
     
     <!-- add code for the form here -->
-    <form action="add_product.php" method="post"
-              id="add_product_form">
+    <form action="add_category.php" method="post"
+              id="add_category_form">
 
             <label>Category:</label>
-            <select name="category_id">
-            <?php foreach ($categories as $category) : ?>
-                <option value="<?php echo $category['categoryID']; ?>">
-                    <?php echo $category['categoryName']; ?>
-                </option>
-            <?php endforeach; ?>
-            </select><br>
+            <input type="text" name="category"><br>
 
-            <label>Code:</label>
-            <input type="text" name="code"><br>
-
-            <label>Name:</label>
-            <input type="text" name="name"><br>
-
-            <label>List Price:</label>
-            <input type="text" name="price"><br>
-
+            
             <label>&nbsp;</label>
-            <input type="submit" value="Add Product"><br>
+            <input type="submit" value="Add"><br>
         </form>
     
     <br>
